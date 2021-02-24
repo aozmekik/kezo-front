b578575275a2a193002db17830446f1d57a07b79
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2021-02-24 14:35:00 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PromotionBarComponent } from './components/promotion-bar/promotion-bar.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PromotionBarComponent,
+    MenuBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
