ae010359160bfa8d43f9dba7f6ee949ad2c325cb
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2021-03-11 20:36:29 +0300

    test

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
+// const express = require('express');
+// const http = require('http');
+// const path = require('path');
 
+// const app = express();
+
+// const port = 3001;
+
+// app.use(express.static(__dirname + '/dist/kezo-front'));
+// console.log(__dirname + '/dist/kezo-front');
+// app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+
+// const server = http.createServer(app);
+
+// server.listen(port, () => console.log('Running...'));
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
 const app = express();
 
-const port = 8080;
+app.use(express.static('./dist/kezo-front'));
 
-app.use(express.static(__dirname + '/dist/kezo-front'));
-console.log(__dirname + '/dist/kezo-front');
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
-const server = http.createServer(app);
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/kezo-front/' }
+    );
+});
 
-server.listen(port, () => console.log('Running...'));
+
+app.listen(8080);
